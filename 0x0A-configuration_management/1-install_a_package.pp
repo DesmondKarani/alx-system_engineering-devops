913cc4d587b32190e61b0383a035d52bf594eafc
Desmond <karanidesmond@protonmail.ch>
2024-02-26 13:24:08 +0300

    dependency

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# This Puppet manifest installs Flask version 2.1.0 using pip3
-package {'Flask':
+# Install pip3 package provider
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install flask from pip3 with version 2.1.0
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
