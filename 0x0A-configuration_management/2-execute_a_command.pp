3357147e0c383c6e5c6ce42b3a93e1c644607599
Desmond <karanidesmond@protonmail.ch>
2024-02-26 13:02:20 +0300

    kill me now

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # This Puppet manifest uses the exec resource to kill a process named 'killmenow'
 
-exec { 'kill_killmenow_process':
-  command   => '/usr/bin/pkill -f killmenow',
-  onlyif    => '/usr/bin/pgrep -f killmenow',
-  path      => ['/bin', '/usr/bin'],
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
