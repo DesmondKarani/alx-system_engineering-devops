c03999837877b471fb677bf2c0bfa65837d80812
Desmond <karanidesmond@protonmail.ch>
2024-03-25 21:29:42 +0300

    pycode

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,49 +3,23 @@
 returns information about his/her TODO list progress'''
 
 import requests
-import argparse
+import sys
 
-
-# Function to fetch user data
-def fetch_user_data(user_id):
-    user_response = requests.get(
-            f"https://jsonplaceholder.typicode.com/users/{user_id}"
-            )
-    if user_response.status_code == 200:
-        return user_response.json()
-    else:
-        return None
-
-
-# Function to fetch TODO data
-def fetch_todo_data(user_id):
-    todos_response = requests.get(
-            f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-            )
-    if todos_response.status_code == 200:
-        return todos_response.json()
-    else:
-        return []  # Return an empty list if there's an error
-
-
-# Main execution
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Fetch and display
-                                     TODO progress for an employee")
-    parser.add_argument("employee_id", type=int, help="ID of the employee")
-    args = parser.parse_args()
+    # JSONPlaceholder API
+    url = "https://jsonplaceholder.typicode.com/"
 
-    user_data = fetch_user_data(args.employee_id)
-    todo_data = fetch_todo_data(args.employee_id)
+    # extract employee data
+    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
 
-    if user_data and todo_data:
-        completed_tasks = sum(todo['completed'] for todo in todo_data)
-        total_tasks = len(todo_data)
+    # to-do list
+    params = {"userId": employee_id}
+    todos = requests.get(url + "todos", params).json()
 
-        print(f"""Employee {user_data['name']} is done with tasks(
-              {completed_tasks}/{total_tasks}):""")
-        for todo in todo_data:
-            if todo['completed']:
-                print(f"\t{todo['title']}")
-    else:
-        print(f"Error: Couldn't fetch data for employee ID {args.employee_id}")
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    [print("\t {}".format(complete)) for complete in completed]
