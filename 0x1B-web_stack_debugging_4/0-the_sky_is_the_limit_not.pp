a1053e6fc8e5a6f0faa0d60fc5eb98c77815cbac
Desmond <karanidesmond@protonmail.ch>
2024-04-27 19:57:38 +0300

    puppet

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,14 @@
-# This block allows the user "holberton" to access and modify a greater number of files simultaneously.
-# It increases the maximum number of files the user "holberton" can have open at one time.
+# This configuration adjusts the hard and soft file limits for the 'holberton' user,
+# enabling better handling of concurrent connections and file operations under load.
 
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => "sed -i '/^holberton hard/ s/4/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin:/bin',
+  command => "/usr/bin/sed -i '/^holberton hard nofile/ s/[0-9]*/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin:/usr/bin',
+  unless  => "/bin/grep -q '^holberton hard nofile 50000' /etc/security/limits.conf",
 }
 
-# This block enhances system flexibility for the "holberton" user by increasing the soft file limit.
-# It raises the initial file descriptor limit for the "holberton" user.
-
 exec { 'increase-soft-file-limit-for-holberton-user':
-  command => "sed -i '/^holberton soft/ s/5/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin:/bin',
+  command => "/usr/bin/sed -i '/^holberton soft nofile/ s/[0-9]*/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin:/bin:/usr/bin',
+  unless  => "/bin/grep -q '^holberton soft nofile 50000' /etc/security/limits.conf",
 }
